99a1b92fafa786aa7969882a1ce2494cf05fb52e
Donny Wu <enderpicture@gmail.com>
2020-12-15 17:13:01 -0800

    adding styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@import 'src/styles/_utilities';
+body {
+    font-family: Ubuntu, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $alWhite;
+    background-color: $alBlack;
+    margin: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,44 @@
+<style scoped lang="scss">
+@import "src/styles/_utilities";
+
+h1 {
+    @include mid-width;
+    font-size: 5rem;
+    text-shadow: 0 2px 2px rgba($alWhite, 0.25), 0 4px 4px rgba($alWhite, 0.25),
+        0 8px 8px rgba($alWhite, 0.25), 0 16px 16px rgba($alWhite, 0.25),
+        0 32px 32px rgba($alWhite, 0.25);
+}
+.file-input {
+    @include mid-width;
+    display: block;
+    height: 5rem;
+    background-color: $alWhite;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    > input {
+        display: none;
+    }
+    > p {
+        color: $alBlack;
+        font-size: 1rem;
+        font-weight: 900;
+    }
+}
+</style>
+
 <template>
-    <h1>Image Utility</h1>
-    <input @change="input" type="file" name="thing" id="" multiple />
-    <file-cell v-for="(file, index) in files" :index="index" :key="file.name" :file="file"></file-cell>
+    <h1>anyform.*</h1>
+    <label class="file-input">
+        <input @change="input" type="file" name="thing" id="" multiple />
+        <p>Add Images Here</p>
+    </label>
+    <file-cell
+        v-for="(file, index) in files"
+        :index="index"
+        :key="file.name"
+        :file="file"
+    ></file-cell>
 </template>
 
 <script>
