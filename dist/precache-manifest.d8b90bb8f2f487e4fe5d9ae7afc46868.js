f41c5d6dee1eafacc83667784a1b9de19a218f8f
Donny Wu <enderpicture@gmail.com>
2020-12-16 01:43:30 -0800

    added dist folder

---
+++ dist/precache-manifest.d8b90bb8f2f487e4fe5d9ae7afc46868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6603893fedba5b14101",
+    "url": "/css/app.4879f88d.css"
+  },
+  {
+    "revision": "42988f0f27d188d4b27b211411917abb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6efd44d3d1bcd5e0a62e",
+    "url": "/js/about.0738c66c.js"
+  },
+  {
+    "revision": "d6603893fedba5b14101",
+    "url": "/js/app.e4ad0e57.js"
+  },
+  {
+    "revision": "99b65c3e5413d63f1b04",
+    "url": "/js/chunk-vendors.eced3a32.js"
+  },
+  {
+    "revision": "ebf98a53a2ccac38ddce8c651dd2d9cf",
+    "url": "/js/img-worker.12136185.worker.js"
+  },
+  {
+    "revision": "36ba2a99edcbdc1c81a3a3dd72bbc44f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
