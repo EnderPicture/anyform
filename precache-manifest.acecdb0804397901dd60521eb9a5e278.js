40c78d2219285e951da1d704379c48d279ebe731
Donny Wu <enderpicture@gmail.com>
2020-12-17 01:14:39 -0800

    new build

---
+++ precache-manifest.acecdb0804397901dd60521eb9a5e278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c555552e0efb324352",
+    "url": "/anyform/css/app.d0e0f0ea.css"
+  },
+  {
+    "revision": "4443e0b2636e900a7b7fc0c808aec25d",
+    "url": "/anyform/index.html"
+  },
+  {
+    "revision": "26234b25bf1b13475526",
+    "url": "/anyform/js/about.d262e34b.js"
+  },
+  {
+    "revision": "60c555552e0efb324352",
+    "url": "/anyform/js/app.ed23ee57.js"
+  },
+  {
+    "revision": "979a76d0c5e7a5400ddf",
+    "url": "/anyform/js/chunk-vendors.7099fba5.js"
+  },
+  {
+    "revision": "69861c1433446870aea74e27022edde9",
+    "url": "/anyform/js/img-worker.8c925575.worker.js"
+  },
+  {
+    "revision": "97ae3510e7dcdeb21a32d0386fa3f61f",
+    "url": "/anyform/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/anyform/robots.txt"
+  }
+]);
