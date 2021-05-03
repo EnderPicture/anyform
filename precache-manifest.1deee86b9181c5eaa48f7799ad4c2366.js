9e6b89ab5b0c836f33d7ea5da9efd38269793ce7
Donny Wu <enderpicture@gmail.com>
2021-05-02 21:37:08 -0700

    quick fix on multiple image file selecter build

---
+++ precache-manifest.1deee86b9181c5eaa48f7799ad4c2366.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60cbbeefbfbdd2f5020",
+    "url": "/anyform/css/app.580d67f8.css"
+  },
+  {
+    "revision": "fbda86669ea588eaf1ae408c1a2edf59",
+    "url": "/anyform/index.html"
+  },
+  {
+    "revision": "059db421f80a63a44ceb",
+    "url": "/anyform/js/about.31186137.js"
+  },
+  {
+    "revision": "f60cbbeefbfbdd2f5020",
+    "url": "/anyform/js/app.34fd618a.js"
+  },
+  {
+    "revision": "575513d0e8472ebcd6a9",
+    "url": "/anyform/js/chunk-vendors.9d3d6191.js"
+  },
+  {
+    "revision": "914a6cefaf34e3474e5510ab73c38e56",
+    "url": "/anyform/js/img-worker.2a1be890.worker.js"
+  },
+  {
+    "revision": "97ae3510e7dcdeb21a32d0386fa3f61f",
+    "url": "/anyform/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/anyform/robots.txt"
+  }
+]);
