12569f6a38d976873992b7a2db222ecc6bb45876
Donny Wu <enderpicture@gmail.com>
2021-05-02 21:37:08 -0700

    quick fix on multiple image file selecter build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anyform/precache-manifest.05c08351debdd3ed4a71261801953a93.js"
+  "/anyform/precache-manifest.1deee86b9181c5eaa48f7799ad4c2366.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "anyform"});
